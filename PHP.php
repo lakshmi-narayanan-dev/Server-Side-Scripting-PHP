3be2ddcf19f4f961e6e0474e3e350026953976ac
LAKSHMI NARAYANAN R <142524733+lakshmi-narayanan-dev@users.noreply.github.com>
2023-12-20 10:41:36 +0530

    Create PHP.php

---
+++ PHP.php
@@ -0,0 +1,28 @@
+
+---------------------------------------------------------------------
+PHP Installation
+---------------------------------------------------------------------
+1. Reference link https://vitux.com/ubuntu-xampp/ 
+
+2. Download xampp from here -> https://www.apachefriends.org/index.html 
+
+3. Open terminal Cd Downloads , Chmod 755
+xampp-linux-x64-8.2.4-0-installer.run (to give read write access)
+
+4. ls -l xampp-linux-x64-8.2.0-0-installer.run to check the access upgradation
+
+5. sudo ./xampp-linux-x64-8.2.0-0-installer.run to run the software kind of
+installation
+
+6. sudo apt install net-tools for monitoring computer network systems
+(sudo rm -r /opt/lampp). 
+rm remove, 
+
+7. sudo /opt/lampp/lampp start run xampp background & check with
+http://localhost/dashboard  browser to check the running correctly.
+
+8. sudo chmod 777 /opt/lampp/htdocs run in terminal to execute all files
+under the folder to read, write and execute access.
+
+9. Paste the dependencies folder in /opt/lampp/htdocs & type
+http://localhost/foldername  view the desired output.
